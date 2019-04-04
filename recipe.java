a56876dda2b8a85d438d891b6e0bb7a24bcf2aff
noogler617 <themolenator@gmail.com>
2019-04-04 13:50:41 -0400

    update

---
+++ recipe.java
@@ -0,0 +1,39 @@
+package recipe;
+  public static void main(String[] args) {
+
+    //Recipe Title
+    System.out.println("\t Bacon Wrapped hamburgers\n");
+
+    //Prep Time
+    System.out.println("Prep: 25 minutes  6 servings  378 cals \n");
+
+    //Ingredients
+    System.out.pringln("Ingredients: \n");
+    System.out.println("\t 1 egg");
+    System.out.println("\t 2 cups of shredded chedder cheese");
+    System.out.println("\t 2 tablspoons grated Parmesan cheese");
+    System.out.println("\t 1 samll onion, choped");
+    System.out.println("\t 1 tablespoon Worcestershire sauce");
+    System.out.println("\t 1/2 teaspoon salt");
+    System.out.println("\t 1/2 teaspoon pepper");
+    System.out.println("\t 1 pound of groud beef");
+    System.out.println("\t 6 slices of bacon");
+    System.out.println("\t 6 hamburger buns split\n");
+
+    //Directions
+    System.out.println("Directions: \n")
+
+    System.out.println("\t Pre-heat grill for high heat.");
+    System.out.println("\t In a large bowl, mix together the Chedder cheese"
+                           + "Parmesan cheese, onion, egg, ketchup,");
+    System.out.println("\t Worcestershire sauce, Salt and Pepper");
+    System.out.println("\t Crumble in the ground beef and mix by hand");
+    System.out.println("\t Secure bacon with toothpicks");
+    System.out.println("\t Form into 6 patties");
+    System.out.println("\t Wrap bacon around each beef pattie");
+    System.out.print("\t Place patties on grill and cook to your desire!\n");
+
+    System.out.println(" Enjoy!")
+
+
+  }
